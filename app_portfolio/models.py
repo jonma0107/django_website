e4038708cd7a2f9100ba22fef41ee3007dc6589b
Jonathan Meza <jonathanmeza@ticsocial.com.co>
2025-02-23 02:32:57 -0500

    [MIGRATE] models app portfolio

---
+++ app_portfolio/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from parler.models import TranslatableModel, TranslatedFields
+
+
+class Project(TranslatableModel):
+  translations = TranslatedFields(
+        description = models.CharField(max_length=250, verbose_name=_("descripcion"))
+    )  
+  title = models.CharField(max_length=150, verbose_name=_("titulo"))  
+  image = models.ImageField(upload_to='portfolio/images/')
+  url = models.URLField(blank=True)
+  demo = models.URLField()
+
+  # def __str__(self):
+  #     return self.title
+  def __unicode__(self):
+        return self.title
+        
+  class Meta:
+        ordering = ['-id']        
+  
+
